5db1e9d429381db93819efed245fab44eb473d17
dakshinatharindu <dakshinatharindu99@gmail.com>
2025-07-10 20:43:52 +0000

    Refactor GenerateVerilog to simplify Verilog generation for a single 8-bit Adder

--- src/main/scala/1_adder/GenerateVerilog.scala
+++ src/main/scala/1_adder/GenerateVerilog.scala
@@ -10,23 +10,9 @@ object GenerateVerilog extends App {
   val dir = new File(targetDir)
   if (!dir.exists()) dir.mkdirs()
   
-  // Generate Verilog for different bit widths
-  def generateVerilog(width: Int, name: String): Unit = {
-    val verilog = ChiselStage.emitSystemVerilog(new Adder(width))
-    
-    val writer = new FileWriter(s"${targetDir}/${name}.v")
-    writer.write(verilog)
-    writer.close()
-    
-    println(s"${width}-bit Adder Verilog generated: ${targetDir}/${name}.v")
-  }
-  
-  // Generate different versions
-  generateVerilog(8, "Adder8bit")
-  generateVerilog(16, "Adder16bit")
-  generateVerilog(32, "Adder32bit")
-  
-  // Also print a 4-bit version to console for quick viewing
-  println("\n--- 4-bit Adder Verilog (console output) ---")
-  println(ChiselStage.emitSystemVerilog(new Adder(4)))
+  val verilog = ChiselStage.emitSystemVerilog(new Adder(8))
+  val verilogFile = new File(s"$targetDir/Adder.v")
+  val writer = new FileWriter(verilogFile)
+  writer.write(verilog)
+  writer.close()
 }
